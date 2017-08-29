7d286bebbf436d26c2fe4b1a9272bea535a5e2ff
Kevin Olson <kolson@paradowski.com>
2017-08-29 13:22:59 -0500

    master: Added server configuration and refactored to a client and server separation.

---
+++ server/app.js
@@ -0,0 +1,26 @@
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+var io = require('socket.io')(app);
+
+io.on('connection', function(socket){
+	io.sockets.emit("user-joined", socket.id, io.engine.clientsCount, Object.keys(io.sockets.clients().sockets));
+
+	socket.on('signal', (toId, message) => {
+		io.to(toId).emit('signal', socket.id, message);
+  	});
+
+    socket.on("message", function(data){
+		io.sockets.emit("broadcast-message", socket.id, data);
+    })
+
+	socket.on('disconnect', function() {
+		io.sockets.emit("user-left", socket.id);
+	})
+});
+
+app.listen(3000, function(){
+	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
